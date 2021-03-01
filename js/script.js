f896691367ea716b5ae848702be8d24c45ab7a23
lfernando_f <lf.fontouraoliveira@gmail.com>
2021-02-28 23:18:27 -0300

    hamburger-transition

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello World! Testando...')
+const btnHamburger = document.querySelector("a.menu");
+console.log(btnHamburger)
+
+btnHamburger.addEventListener('click', function () {
+    console.log('open hamburger');
+    console.log(btnHamburger.classList)
+    if (btnHamburger.classList.contains('open')) {
+        btnHamburger.classList.remove('open');
+    }
+    else {
+        btnHamburger.classList.add('open');
+        console.log('Now there is an open class')
+    }
+})
