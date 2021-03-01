415f4cbc6344d2bf4172200d408ef97564bc054d
lfernando_f <lf.fontouraoliveira@gmail.com>
2021-03-01 07:42:09 -0300

    hamburger-transition

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const btnHamburger = document.querySelector("a.menu");
 const navbar = document.querySelector("nav");
+const headerLinks = document.querySelector('.header-links');
 console.log(btnHamburger)
 
 btnHamburger.addEventListener('click', function () {
@@ -7,9 +8,11 @@ btnHamburger.addEventListener('click', function () {
     console.log(btnHamburger.classList)
     if (navbar.classList.contains('open')) {
         navbar.classList.remove('open');
+        headerLinks.classList.add('has-fade');
     }
     else {
         navbar.classList.add('open');
+        headerLinks.classList.remove('has-fade');
         console.log('Now there is an open class')
     }
 })
