f8d797b7a722eedd3d5fdb374451a455bf3e3fb9
lfernando_f <lf.fontouraoliveira@gmail.com>
2021-03-01 09:31:54 -0300

    overlay

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 const btnHamburger = document.querySelector("a.menu");
-const navbar = document.querySelector("nav");
+const header = document.querySelector("header");
 const headerLinks = document.querySelector('.header-links');
+const overlay = document.querySelector('.overlay');
 console.log(btnHamburger)
 
 btnHamburger.addEventListener('click', function () {
-    console.log('open hamburger');
+    console.log(overlay);
     console.log(btnHamburger.classList)
-    if (navbar.classList.contains('open')) {
-        navbar.classList.remove('open');
-        headerLinks.classList.add('has-fade');
+    if (header.classList.contains('open')) { //fechar o hamburger menu
+        header.classList.remove('open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in')
     }
-    else {
-        navbar.classList.add('open');
-        headerLinks.classList.remove('has-fade');
+    else { //abrir o hamburger
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
         console.log('Now there is an open class')
     }
 })
